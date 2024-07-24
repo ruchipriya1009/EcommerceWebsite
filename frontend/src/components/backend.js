9684a842e69d6e350633efa48d7e2fe7e1610020
Ruchi Priya Kumari <105973023+ruchipriya1009@users.noreply.github.com>
2024-07-24 12:51:42 +0530

    Update backend.js

--- frontend/src/components/backend.js
+++ frontend/src/components/backend.js
@@ -1,4 +1,4 @@
 // export const site = "https://adventurous-wasp-scrubs.cyclic.app"
-export const site = "https://ruby-gorgeous-meerkat.cyclic.cloud"
+export const site = "https://ecombackend-ia3w.onrender.com"
 
 // export const site = "http://localhost:5501"
